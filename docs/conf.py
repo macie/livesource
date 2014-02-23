89d5fb7ddcd433f56633d6daab5eeed9e24cb4bd
maciek <maciek@laptop>
2014-02-23 13:03:12 +0100

    add napoleon extension

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,22 @@ sys.path.insert(0, os.path.abspath('../livesource'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinxcontrib.napoleon',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
 ]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = False
+napoleon_use_rtype = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
